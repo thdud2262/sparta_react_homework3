af86f4d39cb1de539d59ac8b4dfabac7e7d36743
soyoung <thgml1444@naver.com>
2022-02-10 03:25:02 +0900

    오타로 인하여.. 데이터값 안들어옴 찾음

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import { Grid, Text, Image, Button } from '../elements';
-
-
+import { useDispatch } from 'react-redux';
+import { actionCreators as postActions } from "../redux/modules/post";
 
 const Post = (props) => {
-  // console.log(props)
+  // console.log(props) 
+  const dispatch = useDispatch()
+  // 삭제해줘 삭제할거 어떻게가져와 
+
+
+  const delete_btn = ()=> {
+    const imageDel =props
+    // console.log(imageDel, imaged)
+    dispatch(postActions.delPostFB(imageDel))
+  }
+
   return (
     <Grid  border= '1px solid black' padding = '15px 0' margin = '0 0 10px 0 '>
       <Grid is_flex padding = '5px 15px' >   
         <Grid is_flex>
           <Image shape = 'circle'></Image>
-          <Text bold> {props.userinfo.user_name}</Text>
+          <Text bold> {props.user_info.user_name}</Text>
         </Grid>
         <Grid is_flex flex_end >
           <Text margin='0 5px 0 0'> {props.insert_dt} </Text>
-          <Button text='수정'></Button>
+          <Button 
+            text='삭제' 
+            _onClick={()=> {
+              delete_btn()
+            }}
+          ></Button>
         </Grid>
       </Grid>
 
       <Grid padding = '5px 15px' >
-        <Text > {props.comment}</Text>
+        <Text > {props.contents}</Text>
       </Grid>
 
       <Grid>
-        <Image shape ='rectangle'></Image>
+        <Image 
+          shape ='rectangle' 
+          src = {props.image_url} 
+        ></Image>
       </Grid>
 
       <Grid padding = '5px 15px' margin='0 0 20px 0'>
-        <Text bold> {props.comment_cnt} </Text>
+        <Text bold> {props.comment_cnt}개 </Text>
       </Grid>
       
     </Grid>
@@ -36,12 +54,12 @@ const Post = (props) => {
 
 
 Post.defaultProps = {
-  userinfo : { 
+  user_info : { 
     user_name : "소영",
     user_profile : "https://dimg.donga.com/a/600/0/90/5/wps/NEWS/IMAGE/2021/09/27/109431207.2.jpg"
   },
   image_url : "https://dimg.donga.com/a/600/0/90/5/wps/NEWS/IMAGE/2021/09/27/109431207.2.jpg",
-  comment : "안녕하세요! 리액트를 잘하고싶은 정소영입니다 으하하하핳ㅎ  ",
+  contents : "안녕하세요! 리액트를 잘하고싶은 정소영입니다 으하하하핳ㅎ  ",
   comment_cnt: '좋아요 10',
   insert_dt : "2022.02.04"
 
